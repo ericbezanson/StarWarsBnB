dca6c79b6d2048f1accfbef1bc584d18438d143b
ericbezanson <eric.bezanson@outlook.com>
2016-12-20 12:54:48 -0400

    added custom price filter

--- js/controllers/CribsController.js
+++ js/controllers/CribsController.js
@@ -2,6 +2,11 @@
 app.controller('cribsController', ['$scope','cribsFactory', function($scope, cribsFactory) {
     $scope.cribs;
 
+    $scope.priceInfo = {
+        min: 0,
+        max: 1000000000000
+    }
+
     cribsFactory.success(function(data){
         $scope.cribs = data;
     });  
